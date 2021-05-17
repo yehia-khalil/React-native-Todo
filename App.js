e736dbb738dd0c7e3e6bea0725ec72fdae11efe0
Yehia Khalil <yehiakhalilykk@gmail.com>
2021-05-17 12:38:15 +0200

    first

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>To share a photo from your phone with a friend, just press the button below!</Text>
+          <StatusBar style="auto" />  
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: 'white',
+  },
+});
